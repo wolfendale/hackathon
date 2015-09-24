dff07740e4a7d05633afa9dfde87a1d5de725334
Michael Wolfendale <michael.wolfendale@digital.hmrc.gov.uk>
2015-09-24 12:27:39 +0100

    -

--- src/main/java/com/accenture/hackathon/controllers/SentimentController.java
+++ src/main/java/com/accenture/hackathon/controllers/SentimentController.java
@@ -1,7 +1,31 @@
 package com.accenture.hackathon.controllers;
 
-/**
- * Created by michael.wolfendale on 24/09/2015.
- */
+import com.hp.autonomy.iod.client.api.textanalysis.SentimentAnalysisLanguage;
+import com.hp.autonomy.iod.client.api.textanalysis.SentimentAnalysisResponse;
+import com.hp.autonomy.iod.client.api.textanalysis.SentimentAnalysisService;
+import com.hp.autonomy.iod.client.converter.IodConverter;
+import com.hp.autonomy.iod.client.error.IodErrorHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import retrofit.RestAdapter;
+import retrofit.converter.JacksonConverter;
+
+@RestController
 public class SentimentController {
+
+    @RequestMapping("/sentiment")
+    public String sentiment() {
+
+        final RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint("https://api.idolondemand.com/1")
+                .setConverter(new IodConverter(new JacksonConverter()))
+                .setErrorHandler(new IodErrorHandler())
+                .build();
+
+        final SentimentAnalysisService service = restAdapter.create(SentimentAnalysisService.class);
+
+        final SentimentAnalysisResponse response = service.analyzeSentimentForText("26340f38-75ff-458d-8a3d-b66a76582263", "I like cats", SentimentAnalysisLanguage.eng);
+
+        return response.toString();
+    }
 }
