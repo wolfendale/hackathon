0cd4a4ed62323621200dcc4fd72184dc5af25de3
Michael Wolfendale <michael.wolfendale@digital.hmrc.gov.uk>
2015-09-25 14:53:13 +0100

    Added lombok / concept extraction basics

--- src/main/java/com/accenture/hackathon/services/ConceptService.java
+++ src/main/java/com/accenture/hackathon/services/ConceptService.java
@@ -1,8 +1,12 @@
 package com.accenture.hackathon.services;
 
+import com.accenture.hackathon.models.iod.ConceptExtractionResponse;
 import com.accenture.hackathon.services.iod.ConceptExtractionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import retrofit.mime.TypedFile;
+
+import java.io.File;
 
 @Service
 public class ConceptService {
@@ -13,4 +17,8 @@ public class ConceptService {
     }
 
     final private ConceptExtractionService conceptService;
+
+    public ConceptExtractionResponse extractConceptsForFile(File file) {
+        return conceptService.extractConceptsFromFile(new TypedFile("multipart/form-data", file));
+    }
 }
