47f3a740602adaffb6757515eb949f52a95aa261
Michael Wolfendale <michael.wolfendale@digital.hmrc.gov.uk>
2015-09-25 12:31:26 +0100

    Added  more twitter stuff

--- src/main/java/com/accenture/hackathon/services/twitter/TwitterService.java
+++ src/main/java/com/accenture/hackathon/services/twitter/TwitterService.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import twitter4j.*;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 @Service
 public class TwitterService {
 
@@ -15,18 +19,36 @@ public class TwitterService {
 
     final private Twitter twitter;
 
-    public TweetsResponse getTweets(String query) {
-
-        System.out.println(query);
+    public TweetsResponse getTweets(String terms) {
 
         try {
 
-            final QueryResult result = twitter.search(
-                new Query(query)
-            );
+            final List<List<Status>> results = new ArrayList<>();
+            long maxId = 0;
+
+            for (int i = 0; i < 10; i++) {
+
+                final Query query = new Query(terms);
+                query.setCount(100);
+
+                if (i > 0) {
+                    query.setMaxId(maxId);
+                }
+
+                final QueryResult result = twitter.search(query);
+                final List<Status> tweets = result.getTweets();
+                maxId = tweets.get(tweets.size() - 1).getId() - 1;
+                results.add(tweets);
+            }
+
+            final List<Status> tweets = new LinkedList<>();
+
+            for (List<Status> result: results) {
+                tweets.addAll(result);
+            }
 
             return new TweetsResponse(
-                result.getTweets()
+                tweets
             );
         } catch (TwitterException e) {
             return new TweetsResponse(e);
