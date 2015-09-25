cab96ace315a63f56a0c68ca76bef112e36b8f34
Luke Deighton <lukedeighton1@gmail.com>
2015-09-25 13:39:21 +0100

    Updated Colour and Count

--- src/main/java/com/accenture/hackathon/processor/SentimentToColourConverter.java
+++ src/main/java/com/accenture/hackathon/processor/SentimentToColourConverter.java
@@ -8,17 +8,16 @@ public class SentimentToColourConverter {
     private static final int BASE_COLOUR = 120;
 
     public static String generateColor(double sentimentValue){
-
         int r;
         int g;
         int b;
 
         if (sentimentValue > 0) {
             r = (int) (BASE_COLOUR + -BASE_COLOUR * sentimentValue);
-            g = (int) ((255 - BASE_COLOUR) * sentimentValue + BASE_COLOUR);
+            g = (int) ((255 - BASE_COLOUR) * (sentimentValue*sentimentValue) + BASE_COLOUR);
             b = (int) (BASE_COLOUR + -BASE_COLOUR * sentimentValue);
         } else if (sentimentValue < 0) {
-            r = (int) ((255 - BASE_COLOUR) * -sentimentValue + BASE_COLOUR);
+            r = (int) ((255 - BASE_COLOUR) * -(sentimentValue*sentimentValue) + BASE_COLOUR);
             g = (int) (BASE_COLOUR + -BASE_COLOUR * -sentimentValue);
             b = (int) (BASE_COLOUR + -BASE_COLOUR * -sentimentValue);
         } else {
@@ -27,13 +26,6 @@ public class SentimentToColourConverter {
             b = BASE_COLOUR;
         }
 
-        //hacky manipulation to show more neutral looking by squaring the values
-        r = (int) ((r / 255f) * (r / 255f) * 255f);
-        g = (int) ((g / 255f) * (g / 255f) * 255f);
-        b = (int) ((b / 255f) * (b / 255f) * 255f);
-
         return String.format("#%02X%02X%02X", r, g, b);
     }
-
-
 }
